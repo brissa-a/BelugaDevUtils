a33a34e4cc464d61cb347ade671eeabf9660fb33
Alexis Brissard <alexis.brissard@epitech.eu>
2014-06-25 15:35:27 +0900

    Préparation pour le TA4

--- beluga_install.sh
+++ beluga_install.sh
@@ -95,9 +95,9 @@ check_package php5-mysql
 ####################################################
 echo -e "\e[1;93mInstallation of HAXE...\e[0m"
 # Download Haxe
-wget http://www.openfl.org/haxe-3.1.1-linux-installer.tar.gz
+wget http://www.openfl.org/builds/haxe/haxe-3.1.3-linux-installer.tar.gz
 # Installation of Haxe
-tar -xvf haxe-3.1.1-linux-installer.tar.gz
+tar -xvf haxe-3.1.3-linux-installer.tar.gz
 echo "y" | ./install-haxe.sh
 # Configuration of Haxe
 haxe_lib_dir="/usr/lib/haxe"
@@ -108,32 +108,78 @@ echo "${haxe_lib_dir}/lib" | haxelib setup
 # Clean Haxe install
 echo -e "\e[1;93mClean haxe install files.\e[0m"
 rm -rf install-haxe.sh
-rm -rf haxe-3.1.1-linux-installer.tar.gz
+rm -rf hhaxe-3.1.3-linux-installer.tar.gz
 
 ####################################################
 # creation of DIRECTORY ARCHITECTURE
 ####################################################
 echo -e "\e[1;93mDownload of BELUGA...\e[0m"
 # Create architecture in the current directory
-main_dir_name="test"
-beluga_dir_name="beluga"
-demo_dir_name="demo"
-mkdir $main_dir_name
-# Clone repositories in the $main_dir_name
-pushd $main_dir_name
-git clone https://github.com/${git_account}/Beluga.git $beluga_dir_name
-git clone https://github.com/${git_account}/BelugaDemo.git $demo_dir_name
+main_path="$PWD/test"
+
+beluga_path="$main_path/beluga"
+beluga_test_path="$beluga_path/test"
+beluga_www_path="$beluga_path/test/bin/php"
+
+demo_path="$main_dir_path/dominax"
+demo_www_path="$demo_path/bin/php"
+
+
+
+mkdir $main_path
+# Clone repositories in the $main_path
+pushd $main_path
+git clone https://github.com/${git_account}/Beluga.git $beluga_path
+git clone https://github.com/${git_account}/Dominax.git $demo_path
 
 ####################################################
 # installation of BELUGA PROJECT
 ####################################################
 echo -e "\e[1;93mInstallation of BELUGA...\e[0m"
 # Installation of the beluga library
-pushd $beluga_dir_name
+pushd $beluga_path
 haxelib dev beluga .
 haxelib install session
+haxelib install tink_macro
+
+pushd $beluga_test_path
+haxe BelugaTest.hxml
+    pushd $beluga_www_path
+    wget https://raw.github.com/brissa-a/BelugaDevUtils/master/.htaccess
+    popd
 popd
+
 # Compilation of the demo project
-pushd $demo_dir_name
-haxe BelugaDemo.hxml
+pushd $demo_path
+haxe dominax.hxml
+    pushd $demo_www_path
+    wget https://raw.github.com/brissa-a/BelugaDevUtils/master/.htaccess
+    popd
 popd
+
+echo -e "\e[1;93mConfiguring web server\e[0m" 
+####################################################
+# installation of APACHE
+####################################################
+
+hosts="/etc/hosts"
+apacheconf="/etc/apache2/sites-available/default"
+
+#Add hosts
+echo "127.0.0.1       demo.beluga.com" >> hosts
+echo "127.0.0.1       test.beluga.com" >> hosts
+
+echo_virtual_host () {
+    echo "<VirtualHost *>"
+    echo "    ServerName $1"
+    echo "    DocumentRoot $2"
+    echo "</VirtualHost>"
+}
+
+echo "NameVirtualHost *" >> apacheconf
+echo_virtual_host "test.beluga.com" "$beluga_www_path" >> apacheconf
+echo_virtual_host "demo.beluga.com" "$demo_www_path" >> apacheconf
+
+#Check rewrite mod enabled
+a2enmod rewrite
+apachectl restart
